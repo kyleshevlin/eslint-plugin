d8b69c7502a705f545f78e665cd484f712f96817
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2021-02-27 09:09:27 -0800

    helps when you ship the rule

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const preferCustomHooks = require('./rules/prefer-custom-hooks')
+const noSkippedOrFocusedTests = require('./rules/no-skipped-or-focused-tests')
 
 module.exports = {
   rules: {
+    'no-skipped-or-focused-tests': noSkippedOrFocusedTests,
     'prefer-custom-hooks': preferCustomHooks,
   },
 }
